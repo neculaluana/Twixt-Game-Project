a50a7b8febd1bfedf4399f14eea38ef707b494c7
Necula Luana <neculaluana2003@gmail.com>
2023-10-26 20:33:35 +0300

    Player class getters

--- Player.cpp
+++ Player.cpp
@@ -1,2 +1,18 @@
 #pragma once
 #include"Player.h"
+Player::Player(Color color) :
+	m_color(color) {
+}
+Color Player::getColor() const
+{
+	return m_color;
+}
+std::vector<Point> Player::getPoints() const
+{
+	return m_points;
+}
+std::vector<Bridge> Player::getBridges() const
+{
+	return m_bridges;
+}
+;
