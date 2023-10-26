791cbcc812db3cd47f871c9f08c573b647142a5e
lucianamihai <luciana.mihai@student.unitbv.ro>
2023-10-26 19:32:04 +0300

    Bridge Class Private Members

--- Bridge.h
+++ Bridge.h
@@ -1,7 +1,12 @@
+#include "Point.h"
+#include <string>
 class Bridge
 {
 public:
+	Bridge(Point firstPoint, Point secondPoint);
 
 private:
-
+	Point m_firstPoint;
+	Point m_secondPoint;
+	std::string m_color;
 };
