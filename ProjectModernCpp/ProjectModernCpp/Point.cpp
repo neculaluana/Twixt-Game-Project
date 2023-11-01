ee5b7ce023f0d4075852cb80a3f819fc220fbd2b
stefaniapopica <stefania.popica@gmail.com>
2023-11-01 19:21:14 +0200

    Point module created

---
+++ ProjectModernCpp/ProjectModernCpp/Point.cpp
@@ -0,0 +1,23 @@
+module Point;
+
+using namespace twixt;
+Point::Point(int x, int y, Color color)
+	:m_coordinates(std::make_pair(x, y))
+	,m_color(color)
+{}
+
+bool Point::isBridgePossible(const Point& b) const
+{
+	
+	return false;
+}
+
+Color Point::getColor() const
+{
+	return m_color;
+}
+
+std::pair<int, int> Point::getCoordinates() const
+{
+	return m_coordinates;
+}
