55916eb58f7f97ac47fad68623e80c44ad1396b0
stefaniapopica <stefania.popica@gmail.com>
2023-11-15 18:19:18 +0200

    Deleted Point module and recreated Point.h

---
+++ ProjectModernCpp/ProjectModernCpp/Point.h
@@ -0,0 +1,18 @@
+#pragma once
+class Point {
+public:
+	enum class Color : uint8_t
+	{
+		Red,
+		Black
+	};
+	Point(int x, int y, Color color);
+	bool isBridgePossible(const Point& p1, const Point& p2) const;
+	const Color& getColor() const;
+	const std::pair<uint8_t, uint8_t>& getCoordinates() const;
+	~Point() = default;
+
+private:
+	Point::Color m_color : 1;
+	std::pair<uint8_t, uint8_t> m_coordinates;
+};
