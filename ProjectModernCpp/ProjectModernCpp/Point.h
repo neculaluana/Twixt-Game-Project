966dd4efbfa584655cb59a2085774fc4d7b64c95
stefaniapopica <stefania.popica@gmail.com>
2023-11-15 19:44:28 +0200

    Point modified

--- ProjectModernCpp/ProjectModernCpp/Point.h
+++ ProjectModernCpp/ProjectModernCpp/Point.h
@@ -14,6 +14,9 @@ public:
 	const std::pair<uint8_t, uint8_t>& getCoordinates() const;
 	~Point() = default;
 
+	friend bool operator==(const Point& p1, const Point& p2);
+	friend bool operator!=(const Point& p1, const Point& p2);
+
 private:
 	Point::Color m_color : 1;
 	std::pair<uint8_t, uint8_t> m_coordinates;
