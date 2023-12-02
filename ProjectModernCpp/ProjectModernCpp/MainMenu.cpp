ce215219d93966c9e55c2512244acd58ad896427
alexandra-marin29 <alexandra-a.marin@student.unitbv.ro>
2023-12-02 17:09:13 +0200

    MainMenu display method implemented

--- ProjectModernCpp/ProjectModernCpp/MainMenu.cpp
+++ ProjectModernCpp/ProjectModernCpp/MainMenu.cpp
@@ -2,6 +2,32 @@
 #include "Button.h"
 #include <QGraphicsTextItem>
 
+
+
+void MainMenu::start() {
+
+    scene->clear();
+}
+
+void MainMenu::displayMainMenu() {
+
+    Button* playButton = new Button(QString("Play"));
+    int bxPos = this->width() / 2 - playButton->boundingRect().width() / 2;
+    int byPos = 275;
+    playButton->setPos(bxPos, byPos);
+    connect(playButton, SIGNAL(clicked()), this, SLOT(start()));
+    scene->addItem(playButton);
+
+    Button* quitButton = new Button(QString("Quit"));
+    int qxPos = this->width() / 2 - quitButton->boundingRect().width() / 2;
+    int qyPos = 350;
+    quitButton->setPos(qxPos, qyPos);
+    connect(quitButton, SIGNAL(clicked()), this, SLOT(close()));
+    scene->addItem(quitButton);
+
+}
+
+
 MainMenu::MainMenu(QWidget* parent) {
     // set up the screen
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
