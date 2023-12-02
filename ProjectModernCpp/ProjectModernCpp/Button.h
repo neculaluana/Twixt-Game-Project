88542ec1b6af61ed483eaf86b7ce85ef04c5f63e
Necula Luana <neculaluana2003@gmail.com>
2023-12-02 16:47:36 +0200

    defined the structure of the Button class & implemented constructor

--- ProjectModernCpp/ProjectModernCpp/Button.h
+++ ProjectModernCpp/ProjectModernCpp/Button.h
@@ -9,6 +9,11 @@ class Button :public QObject, public QGraphicsRectItem {
     Q_OBJECT
 public:
     Button(QString name, QGraphicsItem* parent = NULL);
+    void mousePressEvent(QGraphicsSceneMouseEvent* event);
+    void hoverEnterEvent(QGraphicsSceneHoverEvent* event);
+    void hoverLeaveEvent(QGraphicsSceneHoverEvent* event);
+signals:
+    void clicked();
 
 private:
     QGraphicsTextItem* text;
