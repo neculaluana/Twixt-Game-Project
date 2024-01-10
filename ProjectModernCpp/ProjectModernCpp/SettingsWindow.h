366f0c6eb54dfaeacc97dd5b1b9b4887e34c5dc5
alexandra-marin29 <alexandra-a.marin@student.unitbv.ro>
2024-01-10 20:22:06 +0200

    settingsWindow class declaration

---
+++ ProjectModernCpp/ProjectModernCpp/SettingsWindow.h
@@ -0,0 +1,15 @@
+#include <QDialog>
+#include <QSpinBox>
+#include <QPushButton>
+#include <QFormLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+
+class SettingsWindow : public QDialog
+{
+	Q_OBJECT
+public:
+
+
+};
+
